4be601a841ce9f8ecc0eceb6b71fcf6d41bfae58
Pavel Demin <paveldemin@me.com>
2022-12-13 16:37:56 +0800

    feat: add icon button

--- shiny_semantic/elements/button.py
+++ shiny_semantic/elements/button.py
@@ -5,14 +5,27 @@ from ..typings import ButtonColor, ButtonFill, ButtonSize, ButtonType
 
 def button(
     input_id: str,
-    label: str,
+    label: str = "",
+    icon_name: str = "",
     type: ButtonType = ButtonType.default,
     fill: ButtonFill = ButtonFill.solid,
     color: ButtonColor = ButtonColor.default,
     size: ButtonSize = ButtonSize.default,
 ):
+
+    icon = None
+    button_icon_class = ""
+
+    if icon_name != "":
+        icon = tags.i(class_=f"{icon_name} icon")
+        button_icon_class = "icon"
+        if label != "":
+            button_icon_class += " labeled"
+
     classname = (
-        f"{size.value} ui {color.value} {fill.value} {type.value} button "
+        f"{size.value} ui {color.value} "
+        f"{fill.value} {button_icon_class} "
+        f"{type.value} button "
     )
 
     return tags.button(
@@ -20,5 +33,6 @@ def button(
             "id": input_id,
             "class": classname,
         },
+        icon,
         label,
     )
