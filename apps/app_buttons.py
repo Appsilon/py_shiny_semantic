d8876ff35e56d83af53a793ee005c1902643b7fc
Pavel Demin <paveldemin@me.com>
2022-12-13 15:43:31 +0800

    chore: update demo app

--- apps/app_buttons.py
+++ apps/app_buttons.py
@@ -1,6 +1,8 @@
 from shiny import App, render, ui
 
-from shiny_semantic import input_action_button, page_semantic
+from shiny_semantic import page_semantic
+from shiny_semantic.elements import button
+from shiny_semantic.typings import ButtonType
 
 
 def click_reporter(btn_class, output_id):
@@ -15,41 +17,27 @@ def click_reporter(btn_class, output_id):
 
 app_ui = page_semantic(
     ui.tags.div(
-        input_action_button("btn_default", "Default"),
-        input_action_button("btn_primary", "Primary", "primary"),
-        input_action_button("btn_secondary", "Secondary", "secondary"),
-        input_action_button("btn_basic", "Basic", "basic"),
+        button("btn_default", "Default"),
+        button("btn_primary", "Primary", ButtonType.primary),
     ),
     ui.tags.div(
         click_reporter("Default", "btn_default_output"),
         click_reporter("Primary", "btn_primary_output"),
-        click_reporter("Secondary", "btn_secondary_output"),
-        click_reporter("Basic", "btn_basic_output"),
     ),
     title="Example: Buttons",
 )
 
 
 def server(input, output, session):
-    @output
+    @output(id="btn_default_output")
     @render.text
-    def btn_default_output():
+    def _():
         return input.btn_default()
 
-    @output
+    @output(id="btn_primary_output")
     @render.text
-    def btn_primary_output():
+    def _():
         return input.btn_primary()
-
-    @output
-    @render.text
-    def btn_secondary_output():
-        return input.btn_secondary()
-
-    @output
-    @render.text
-    def btn_basic_output():
-        return input.btn_basic()
 
 
 app = App(app_ui, server)
