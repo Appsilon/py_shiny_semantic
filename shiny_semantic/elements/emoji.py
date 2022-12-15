cc5e2a26d332e2a3d54a24d56c21c830bc3b367e
Pavel Demin <paveldemin@me.com>
2022-12-15 18:25:08 +0800

    refactor: remove default class value from Emoji
    It is bad to have a default class = medium,
    because it will affect the autosizing behaviour of a component.

--- shiny_semantic/elements/emoji.py
+++ shiny_semantic/elements/emoji.py
@@ -5,6 +5,6 @@ from shiny.ui import tags
 
 def emoji(
     emoji_name: str,
-    class_name: Optional[str] = "medium",
+    class_name: Optional[str] = "",
 ):
     return tags.em({"data-emoji": f":{emoji_name}:", "class": class_name})
