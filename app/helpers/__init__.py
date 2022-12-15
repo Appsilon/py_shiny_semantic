19e65eee84f30dca16f07f4946ec1f7dfc292be1
Pavel Demin <paveldemin@me.com>
2022-12-15 17:12:47 +0800

    feat: feature section & subsection

--- app/helpers/__init__.py
+++ app/helpers/__init__.py
@@ -1,2 +1,8 @@
-__all__ = ("hero",)
+__all__ = (
+    "feature_section",
+    "feature_subsection",
+    "hero",
+)
+from .feature_section import feature_section
+from .feature_subsection import feature_subsection
 from .hero import hero
