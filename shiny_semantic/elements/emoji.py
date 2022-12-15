242c11920f06b127b5692e009885a995b98c375f
Pavel Demin <paveldemin@me.com>
2022-12-15 18:33:22 +0800

    fix: adjust emoji test

--- shiny_semantic/elements/emoji.py
+++ shiny_semantic/elements/emoji.py
@@ -5,6 +5,6 @@ from shiny.ui import tags
 
 def emoji(
     emoji_name: str,
-    class_name: Optional[str] = "",
+    class_name: Optional[str] = None,
 ):
     return tags.em({"data-emoji": f":{emoji_name}:", "class": class_name})
