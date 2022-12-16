b89b4b57a0c929a495c4d4dd40d128f60c877b95
Pavel Demin <paveldemin@me.com>
2022-12-16 14:58:19 +0800

    feat: extend input to support icon and label

--- shiny_semantic/elements/input.py
+++ shiny_semantic/elements/input.py
@@ -3,13 +3,34 @@ from typing import Optional
 from shiny._namespaces import resolve_id
 from shiny.ui import tags
 
+from shiny_semantic.elements import icon
 
-def text_input(input_id: str, placeholder: Optional[str] = ""):
+
+def text_input(
+    input_id: str,
+    placeholder: Optional[str] = "",
+    input_type: Optional[str] = "text",
+    class_name: str = "",
+    icon_name: Optional[str] = None,
+    label: Optional[str] = None,
+):
+    icon_tag = None
+    if icon_name is not None:
+        icon_tag = icon(icon_name)
+        class_name += " icon"
+
+    label_tag = None
+    if label is not None:
+        label_tag = tags.div(label, class_="ui label")
+        class_name += " labeled"
+
     return tags.div(
+        label_tag,
         tags.input(
-            type="text",
+            type=input_type,
             placeholder=placeholder,
             id=resolve_id(input_id),
         ),
-        class_="ui input",
+        icon_tag,
+        class_=f"ui {class_name} input",
     )
