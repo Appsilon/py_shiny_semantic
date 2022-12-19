5005e1c8a2e43fafc657da9f07f4c02d360e352b
Pavel Demin <paveldemin@me.com>
2022-12-19 15:42:10 +0800

    feat: transfer shiny bindings

--- shiny_semantic/www/shiny-semantic-bindings.js
+++ shiny_semantic/www/shiny-semantic-bindings.js
@@ -66,3 +66,27 @@ $.extend(semanticButtonBinding, {
   },
 });
 Shiny.inputBindings.register(semanticButtonBinding, "shiny.semanticButton");
+
+$.fn.modal.settings.onShow = function () {
+  Shiny.bindAll();
+};
+
+Shiny.addCustomMessageHandler("showSemanticModal", function (message) {
+  $("#" + message.id).modal(message.action);
+});
+
+Shiny.initSemanticModal = function (id) {
+  $('[id="' + id + '"]:not(:first)').remove();
+};
+
+Shiny.addCustomMessageHandler("createSemanticModal", function (message) {
+  let id = $(message.ui_modal).modal().attr("id");
+
+  Shiny.initSemanticModal(id);
+
+  $("#" + id).modal(message.action);
+});
+
+Shiny.addCustomMessageHandler("hideAllSemanticModals", function (message) {
+  $(".ui .modal").modal("hide");
+});
