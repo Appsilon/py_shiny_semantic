00094623b0434d7be296ffa4338f23168266ab66
Pavel Demin <paveldemin@me.com>
2022-12-28 17:38:55 +0800

    feat: transfer shiny binding

--- shiny_semantic/www/shiny-semantic-bindings.js
+++ shiny_semantic/www/shiny-semantic-bindings.js
@@ -93,3 +93,125 @@ Shiny.addCustomMessageHandler("showSemanticModal", (payload) => {
   // Not first time: find existing modal and invoke it
   $modal.modal("show");
 });
+
+/**
+ * Semantic slider binding
+ */
+var semanticSliderBinding = new Shiny.InputBinding();
+
+$.extend(semanticSliderBinding, {
+  // This initialize input element. It extracts data-value attribute and use that as value.
+  initialize: function (el) {
+    // TODO(pavel): this initialize does not match the original R/Shiny binding
+    // and it will not work with custom ticks, e.g. letters
+    const {
+      min,
+      max,
+      start,
+      step,
+      end,
+      showlabelticks: showLabelTicks,
+    } = $(el).data();
+
+    const onChange = (_value) => $(el).trigger("change");
+
+    $(el).slider({ min, max, start, step, end, showLabelTicks, onChange });
+  },
+
+  // This returns a jQuery object with the DOM element.
+  find: function (scope) {
+    // checkbox with type slider was also found here causing: https://github.com/Appsilon/shiny.semantic/issues/229
+    return $(scope).find(".ss-slider");
+  },
+
+  // Returns the ID of the DOM element.
+  getId: function (el) {
+    return el.id;
+  },
+
+  // Given the DOM element for the input, return the value as JSON.
+  getValue: function (el) {
+    let value = $(el).slider("get value");
+    // Takes either one or two arguments depending on if it's a range or normal slider
+    if ($(el).hasClass("range")) {
+      value = [
+        $(el).slider("get thumbValue", "first"),
+        $(el).slider("get thumbValue", "second"),
+      ];
+    }
+
+    if ($(el).data("ticks")) {
+      return $(el).data("ticks")[value];
+    } else {
+      return value;
+    }
+  },
+  getType: function (el) {
+    if ($(el).data("ticks")) {
+      return false;
+    } else {
+      return "shiny.number";
+    }
+  },
+  // Given the DOM element for the input, set the value.
+  setValue: function (el, value) {
+    if ($(el).data("ticks")) {
+      if ($(el).hasClass("range")) {
+        $(el).slider(
+          "set rangeValue",
+          $(el).data("ticks").indexOf(value[0]),
+          $(el).data("ticks").indexOf(value[1]),
+        );
+      } else {
+        $(el).slider("set value", $(el).data("ticks").indexOf(value[0]));
+      }
+    } else {
+      if ($(el).hasClass("range")) {
+        $(el).slider("set rangeValue", value[0], value[1]);
+      } else {
+        $(el).slider("set value", value[0]);
+      }
+    }
+  },
+
+  // Set up the event listeners so that interactions with the
+  // input will result in data being sent to server.
+  // callback is a function that queues data to be sent to
+  // the server.
+  subscribe: function (el, callback) {
+    $(el).on("keyup change", function () {
+      callback(true);
+    });
+  },
+
+  // TODO: Remove the event listeners.
+  unsubscribe: function (el) {
+    $(el).off(".semanticSliderBinding");
+  },
+
+  // This returns a full description of the input's state.
+  getState: function (el) {
+    return {
+      value: this.getValue(el),
+    };
+  },
+
+  // The input rate limiting policy.
+  getRatePolicy: function () {
+    return {
+      // Can be 'debounce' or 'throttle':
+      policy: "debounce",
+      delay: 50,
+    };
+  },
+
+  receiveMessage: function (el, data) {
+    if (data.hasOwnProperty("value")) {
+      this.setValue(el, data.value);
+    }
+
+    $(el).trigger("change");
+  },
+});
+
+Shiny.inputBindings.register(semanticSliderBinding, "shiny.semanticSlider");
